30959298d0b1818e1383673a2773069cd10f57c2
DanielAArif <daniel.arif@mhs.unsoed.ac.id>
2023-11-15 19:50:57 +0700

    Update login.php
    Memindahkan kode html ke bawah

--- halaman/login.php
+++ halaman/login.php
@@ -1,30 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel ="stylesheet" type="text/css"  href="../style/login.css">
-    <title>Log In</title>
-</head>
-<body>
-    <div class="box">
-        <h2>LOGIN ACCOUNT</h2>
-        <form>
-            <div class="input-box">
-                <input type="text" name="email" placeholder="Email" >
-            </div>
-            <div class="input-box">
-                <input type="password" name="password" placeholder="Password">
-            </div>
-            <div class="submit-box">
-                <button type="submit" name="submit">Log in Account</button>
-            </div>
-            <p>Don't have an account? <a href="register.php">Register here</a></p>
-        </form>        
-    </div>
-</body>
-</html>
-
 <?php
     include("koneksi.php");
     if(isset($_POST['submit'])){
@@ -49,3 +22,30 @@
             echo "username tidak ditemukan";
         }}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel ="stylesheet" type="text/css"  href="../style/login.css">
+    <title>Log In</title>
+</head>
+<body>
+    <div class="box">
+        <h2>LOGIN ACCOUNT</h2>
+        <form>
+            <div class="input-box">
+                <input type="text" name="email" placeholder="Email" >
+            </div>
+            <div class="input-box">
+                <input type="password" name="password" placeholder="Password">
+            </div>
+            <div class="submit-box">
+                <button type="submit" name="submit">Log in Account</button>
+            </div>
+            <p>Don't have an account? <a href="registrasi.php" style="color: #BE3144; text-decoration: none;">Register here</a></p>
+        </form>        
+    </div>
+</body>
+</html>
