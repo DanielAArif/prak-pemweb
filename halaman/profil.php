e47d9d28e2b770d2a70de8ea9c751ea52514c513
Leppi <levimuhammad101@gmail.com>
2023-11-27 02:18:22 +0700

    Update profil.php
    penambahan delete,select, dan update password + penambahan session

--- halaman/profil.php
+++ halaman/profil.php
@@ -1,3 +1,43 @@
+<?php
+    include("koneksi.php");
+    session_start();
+
+    //delete
+    // Cek apakah formulir dikirimkan untuk menghapus akun
+    if(isset($_POST['hapus_akun'])) {
+        $id = $_POST['id_account'];
+
+        $query = "DELETE FROM user WHERE id= '$id'";
+        $result = mysqli_query($koneksi, $query);
+        if($result) {
+            echo "<script>alert('Berhasil menghapus akun!'); window.location.href = 'login.php'</script>";
+            exit();
+        } else {
+            echo "<script>alert('Gagal menghapus akun!');";        
+        }
+    }
+
+    if (isset($_POST['submit'])){
+        $id = $_SESSION['id'];
+        $password = $_POST['password'];
+        $cpassword = $_POST['confirm_password'];
+
+        if ($password !== $cpassword) {
+            echo "Passwords dan confirm password tidak sama";
+        } else {
+            $query = "UPDATE `user` SET password='$password' WHERE id='$id'";
+            $result = mysqli_query($koneksi, $query);
+            if ($result) {
+                echo "<script> alert ('berhasil mengubah password!'); window.location.href = 'profil.php'</script>";
+                header("Location: profil.php");
+            } else {
+                echo "Pembaruan gagal: " . mysqli_error($koneksi);
+            }
+        }
+    }
+?>
+    
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,34 +58,41 @@
             <li><a href="profil.php"><img src="../gambar/profil1.png"/></a></li>
         </ul>
     </nav>
+
+
     <div class="bg">
     <div class="box" style="height: 700px">
         <div class="account-info">
             <h2>ACCOUNT INFORMATION</h2>
-            <form action="POST">
+            <form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
+                <input type="hidden" name="id_account" value="<?php echo $_SESSION['id'];?>">
                 <div class="username">
                     <div class="name-u">Username</div>
                     <div class="dot">:</div>
-                    <div class="input-box"><input type="text" name="username" readonly><br/></div>
+                    <div class="input-box"><input type="text" name="username" value="<?php echo $_SESSION['username']; ?>" readonly><br/></div>
                 </div>
                 <div class="email">
                     <div class="name-e">Email</div>
                     <div class="dot">:</div>
-                    <div class="input-box"><input type="text" name="email" readonly><br/></div>
+                    <div class="input-box"><input type="text" name="email" value="<?php echo $_SESSION['email']; ?>" readonly><br/></div>
                 </div>
+                <br>
+                <div class="submit-box"><button type="submit" name="hapus_akun" style="padding: 15px 30px;" onclick="return confirm('Are you sure want to delete account?')">Hapus Akun</button></div>
+                <hr>
             </form>
-            <br>
-            <div class="submit-box"><button type="submit" name="hapus_akun" style="padding: 15px 30px;" onclick="return hapus_akun()">Hapus Akun</button></div>
-            <hr>
+
+
         </div>
         <div class="change-pass">
             <h2>CHANGE PASSWORD</h2>
-            <form method="POST" action="">
+            <form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>">
                 <div class="input-pass"><input type="password" name="password" placeholder="Password"><br/></div>
                 <div class="input-pass"><input type="password" name="confirm_password" placeholder="Confirm Password"><br/></div>
-                <div class="submit-box"><button type="submit" name="submit" style="padding: 15px 40px;">Confirm</button></div>
+                <div class="submit-box"><button type="submit" name="submit" style="padding: 15px 40px;" onclick="return confirm('Are you sure want to delete account?')">Confirm</button></div>
             </form>
         </div>
+
+
     </div>
     </div>
 </div>
