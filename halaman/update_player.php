d76642412472a72c10981088b55af2fdc4c4ec3a
Leppi <levimuhammad101@gmail.com>
2023-11-28 01:58:55 +0700

    Update update_player.php
    penambahan update,delete dan select untuk menampilkan data sebelumnya

--- halaman/update_player.php
+++ halaman/update_player.php
@@ -2,39 +2,46 @@
     include("koneksi.php");
     session_start();
 
-    if(isset($_POST['editid'])){
-        if (isset($_POST['update'])){
-            $no_punggung = $_POST['number'];
-            $nama = $_POST['name'];
-            $role = $_POST['position'];
-            $negara = $_POST['country'];
-            $umur = $_POST['age'];
-            $height = $_POST['tinggibadan'];
-            $weight = $_POST['beratbadan'];
-            $foto = $_POST['pic'];
-
-            $query = "UPDATE `player` SET no_punggung='$number', nama='$name', role='$position', negara='$country, umur='$age', tinggibadan='$height', beratbadan='$weight', foto='$pic'  WHERE no_punggung='$number'";
-            $result = mysqli_query($koneksi, $query);
-                if ($result) {
-                    echo "Edit successful";
-                    header("Location: player.php");
-                } else {
-                    echo "Pembaruan gagal: " . mysqli_error($koneksi);
-                }
-        }
+    if(!isset($_SESSION['login'])){
+        header("location: login.php");
+        exit;
     }
-        if(isset($_POST['editid'])){
-            if(isset($_POST['delete'])) {
-                $no_punggung = $_POST['number'];
+    $no_punggung = $_GET['editid'];
+    $query = "SELECT * FROM `pemain` WHERE no_punggung='$no_punggung'";
+    $result = mysqli_query($koneksi,$query);
+    $data_user = mysqli_fetch_assoc($result);
 
-                $query = "DELETE FROM player WHERE no_punggung= '$no_punggung'";
-                $result = mysqli_query($koneksi, $query);
-                if($result) {
-                    echo "<script>alert('Berhasil menghapus pemain!'); window.location.href = 'player.php'</script>";
-                    exit();
-                } else {
-                    echo "<script>alert('Gagal menghapus pemain!');";        
-                }
+
+
+    if (isset($_POST['update'])){
+        $no_punggung = $_POST['number'];
+        $nama = $_POST['name'];
+        $role = $_POST['position'];
+        $negara = $_POST['country'];
+        $umur = $_POST['age'];
+        $height = $_POST['height'];
+        $weight = $_POST['weight'];
+        $foto = $_POST['pic'];
+
+        $query = "UPDATE `pemain` SET no_punggung='$no_punggung', nama='$nama', role='$role', negara='$negara', umur='$umur', tinggibadan='$height', beratbadan='$weight', foto='$foto'  WHERE no_punggung='$no_punggung'";
+        $result = mysqli_query($koneksi, $query);
+            if ($result) {
+                echo "Edit successful";
+                header("Location: player.php");
+            } else {
+                echo "Pembaruan gagal: " . mysqli_error($koneksi);
+            }
+        }
+        if(isset($_POST['delete'])) {
+            $no_punggung = $_GET['editid'];
+
+            $query = "DELETE FROM pemain WHERE no_punggung= '$no_punggung'";
+            $result = mysqli_query($koneksi, $query);
+            if($result) {
+                echo "<script>alert('Berhasil menghapus pemain!'); window.location.href = 'player.php'</script>";
+                exit();
+            } else {
+                echo "<script>alert('Gagal menghapus pemain!');";        
             }
         }
 ?>
@@ -64,46 +71,46 @@
     <div class="box" style="height: 850px">
         <div class="account-info">
             <h2>UPDATE PLAYER DETAILS</h2>
-            <form action="POST">
+            <form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
                 <div class="name">
                     <div class="name_">Name</div>
                     <div class="dot">:</div>
-                    <div class="input-box"><input type="text" name="name"><br/></div>
+                    <div class="input-box"><input type="text" name="name" value="<?php echo $data_user['nama']; ?>"><br/></div>
                 </div>
                 <div class="number">
                     <div class="number_">Jersey Number</div>
                     <div class="dot">:</div>
-                    <div class="input-box"><input type="text" name="number"><br/></div>
+                    <div class="input-box"><input type="text" name="number" value="<?php echo $data_user['no_punggung']; ?>"><br/></div>
                 </div>
                 <div class="country">
                     <div class="country_">Country</div>
                     <div class="dot">:</div>
-                    <div class="input-box"><input type="text" name="country"><br/></div>
+                    <div class="input-box"><input type="text" name="country" value="<?php echo $data_user['negara']; ?>"><br/></div>
                 </div>
                 <div class="age">
                     <div class="age_">Age</div>
                     <div class="dot">:</div>
-                    <div class="input-box"><input type="text" name="age"><br/></div>
+                    <div class="input-box"><input type="text" name="age" value="<?php echo $data_user['umur']; ?>"><br/></div>
                 </div>
                 <div class="position">
                     <div class="position_">Position</div>
                     <div class="dot">:</div>
-                    <div class="input-box"><input type="text" name="position"><br/></div>
+                    <div class="input-box"><input type="text" name="position" value="<?php echo $data_user['role']; ?>"><br/></div>
                 </div>
                 <div class="height">
                     <div class="height_">Height</div>
                     <div class="dot">:</div>
-                    <div class="input-box"><input type="text" name="height"><br/></div>
+                    <div class="input-box"><input type="text" name="height" value="<?php echo $data_user['tinggibadan']; ?>"><br/></div>
                 </div>
                 <div class="weight">
                     <div class="weight_">Weight</div>
                     <div class="dot">:</div>
-                    <div class="input-box"><input type="text" name="weight"><br/></div>
+                    <div class="input-box"><input type="text" name="weight" value="<?php echo $data_user['beratbadan']; ?>"><br/></div>
                 </div>
                 <div class="pic">
                     <div class="pic_">Insert Picture</div>
                     <div class="dot">:</div>
-                    <div id="pic-box"><input type="file" name="pic" class="pic-box"><br/></div>
+                    <div id="pic-box"><input type="file" name="pic" class="pic-box" value="<?php echo $data_user['foto']; ?>"><br/></div>
                 </div>
                 <div class="submit-box">
                     <button type="submit" name="update" style="padding: 15px 30px;">Update</button>
